30f7dd6cb4d4fa1bc50bf66ce32d8ccb910c0fd9
Pavel Dubrova <pashadubrova@gmail.com>
2019-11-17 00:35:48 +0200

    gralloc_legacy: Add legacy gralloc headers and selector
    Add legacy gralloc headers that are necessary for the correct
    operation of legacy gralloc and other system components that
    depend on these headers.
    These headers as well as legacy gralloc itself are based on
    LA.UM.8.2.r1-04800-sdm660.0 tag.
    
    Also based on the fact that we cannot use conditions in blueprints,
    add a gralloc selector handler which was written on Go and was
    added to this repo to correctly select the headers we need.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

---
+++ gralloc_selector.go
@@ -0,0 +1,55 @@
+// Copyright (C) 2019 Pavel Dubrova <pashadubrova@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package gralloc_selector
+
+import (
+	"android/soong/android"
+	"android/soong/cc"
+)
+
+func init() {
+	android.RegisterModuleType("gralloc_selector", gralloc_DefaultsFactory)
+}
+
+func gralloc_DefaultsFactory() android.Module {
+	module := cc.DefaultsFactory()
+	android.AddLoadHook(module, gralloc_Defaults)
+
+	return module
+}
+
+func gralloc_Defaults(ctx android.LoadHookContext) {
+	type props struct {
+		// List of directories to export as headers
+		Export_include_dirs []string
+	}
+
+	p := &props{}
+	p.Export_include_dirs = exportDir(ctx)
+
+	ctx.AppendProperties(p)
+}
+
+func exportDir(ctx android.BaseContext) []string {
+	var export_include_dirs []string
+
+	if ctx.DeviceConfig().TargetUsesGralloc1() {
+		export_include_dirs = append(export_include_dirs, "gralloc_legacy")
+	} else {
+		export_include_dirs = append(export_include_dirs, "gralloc")
+	}
+
+	return export_include_dirs
+}
